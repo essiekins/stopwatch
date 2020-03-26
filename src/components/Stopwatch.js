c538ecc385b6f9490a2e2965616b72c75ee49055
Your Name <Student_MA117@Student-MA117.local>
2020-03-26 11:45:40 -0400

    figured out where starting state should live

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -5,33 +5,22 @@ import React from 'react';
 {/* render function first? */}
 {/*  log seconds first using Date.prototype.getSeconds() in a state counting from 0 */}
 {/* realized rendering visual components should be separate */}
-{/*according to examples on w3schools, my starting state should be under class start}
+{/*according to examples on w3schools, my starting state should be under class start */}
+{/* https://www.w3schools.com/react/react_state.asp */}
 
 class Stopwatch extends Component {
+    offState={   //starting at off state, everything should be 0
+        on=false,
+        start=0,
+        time=0,
+    }
     render(){
         return (
             <div className="Stopwatch">
-
+           
             </div>
         )
     }
 }
-
-    /* off state to start off, then an on switch? probably need a running one separate from the on */
-        offState={        
-        on=false, /*on switch should be off at start */
-        start=0,  /* start counting at 0 */
-        time=0,   /* time starts at 0. adding in case time and date method needs remove later */
-        }
-
-    /*on switch here */
-        timeStart={
-            on=true, /* when pressed time starts, switch on */
-            
-
-        }
-
-        // don't forget to make an off one!
-    
 
 export default Stopwatch;
