dfb07c9261f58834b68eb08ec648ee4cc876f8d1
Your Name <Student_MA117@Student-MA117.local>
2020-03-26 02:21:50 -0400

    creating separate functions for Stopwatch's visual render and a function for startState)

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -4,21 +4,16 @@ import React from 'react';
 {/* timer needs a reset state and a stop state */ }
 {/* render function first? *?}
 {/*  log seconds first using Date.prototype.getSeconds() in a state counting from 0 */}
-
+{/* realized rendering visual components should be separate */}
 
 function Stopwatch() {
-    rednder() {
-        return (
-            <div className="watchBox">
+            rednder() {
+                 return (
+                  <div className="watchBox">
 
-                {/*trying to create a state for start up, using time methods */}
-                this.set state({
-                    on = true,
-                    time = this.setState.time,
-                    countStart =   
-    })
-
-            </div>
+                    
+             })
+                </div>
         )
     }
 }
