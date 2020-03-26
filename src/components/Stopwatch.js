81a848ed186a3e419a1c00e6aab7b6662ad8a3f5
Your Name <Student_MA117@Student-MA117.local>
2020-03-26 11:30:05 -0400

    figuring out where the start state goes

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -2,20 +2,20 @@ import React from 'react';
 
 {/* my timer starts at 0 and counts up */ }
 {/* timer needs a reset state and a stop state */ }
-{/* render function first? *?}
+{/* render function first? */}
 {/*  log seconds first using Date.prototype.getSeconds() in a state counting from 0 */}
 {/* realized rendering visual components should be separate */}
+{/*according to examples on w3schools, my starting state should be under class start}
 
-Stopwatch=() =>{
-     return(
-     <div className="Stopwatch">
-         
-     
-     </div>
-                
-     )
+class Stopwatch extends Component {
+    render(){
+        return (
+            <div className="Stopwatch">
 
+            </div>
+        )
     }
+}
 
     /* off state to start off, then an on switch? probably need a running one separate from the on */
         offState={        
@@ -24,9 +24,10 @@ Stopwatch=() =>{
         time=0,   /* time starts at 0. adding in case time and date method needs remove later */
         }
 
+    /*on switch here */
         timeStart={
             on=true, /* when pressed time starts, switch on */
-
+            
 
         }
 
