69a9168ec3c33c3080311d4e4d63390efe22bc16
Your Name <Student_MA117@Student-MA117.local>
2020-03-26 02:12:08 -0400

    trying to figure out how to render

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
+{/* my timer starts at 0 and counts up */}
+{/* timer needs a reset state and a stop state */}
+{/*  log seconds first using Date.prototype.getSeconds() in a state counting from 0 */}
+
+
 function Stopwatch(){
     return(
         <div className="watchBox">
-        {/* my timer starts at 0 and counts up */}
-        {/* timer needs a reset state and a stop state */}
+     
+        this.set state({
+            on= true,
+            time= this.setState.time,
+            start= 
+        })
         
         </div>
     )
 }
+
 export default Stopwatch;
