2cfd972c38f0f8ca0899a984af9a85ca836a3f85
Your Name <Student_MA117@Student-MA117.local>
2020-03-26 01:58:37 -0400

    pseudoing firsts steps

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -3,6 +3,8 @@ import React from 'react';
 function Stopwatch(){
     return(
         <div className="watchBox">
+        {/* my timer starts at 0 and counts up */}
+        {/* timer needs a reset state and a stop state */}
         
         </div>
     )
