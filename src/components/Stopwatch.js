5bba5bedb80687b4e4b4cc404d8e8da17cb202f0
Your Name <Student_MA117@Student-MA117.local>
2020-03-26 02:48:53 -0400

    creating off and on states for the watch

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -7,15 +7,25 @@ import React from 'react';
 {/* realized rendering visual components should be separate */}
 
 function Stopwatch() {
-            rednder() {
-                 return (
-                  <div className="watchBox">
+     return(
+     <div className="watchBox">
+         
+     
+     </div>
+                
+     )
 
-                    
-             })
-                </div>
-        )
     }
-}
+        offState={        
+        on=false, /*on switch should be off at start */
+        start=0,  /* start counting at 0 */
+        time=0,   /* time starts at 0. adding in case time and date method needs remove later */
+        }
+
+        timeStart={
+            on=true, /* when pressed time starts, switch on */
+            
+        }
+    
 
 export default Stopwatch;
