4b9ed8dd5523bd519eba3b2dac0e041c7b3d65a8
Your Name <Student_MA117@Student-MA117.local>
2020-03-26 02:18:33 -0400

    working on start state

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,22 +1,26 @@
 import React from 'react';
 
-{/* my timer starts at 0 and counts up */}
-{/* timer needs a reset state and a stop state */}
+{/* my timer starts at 0 and counts up */ }
+{/* timer needs a reset state and a stop state */ }
+{/* render function first? *?}
 {/*  log seconds first using Date.prototype.getSeconds() in a state counting from 0 */}
 
 
-function Stopwatch(){
-    return(
-        <div className="watchBox">
-     
-        this.set state({
-            on= true,
-            time= this.setState.time,
-            start= 
-        })
-        
-        </div>
-    )
+function Stopwatch() {
+    rednder() {
+        return (
+            <div className="watchBox">
+
+                {/*trying to create a state for start up, using time methods */}
+                this.set state({
+                    on = true,
+                    time = this.setState.time,
+                    countStart =   
+    })
+
+            </div>
+        )
+    }
 }
 
 export default Stopwatch;
