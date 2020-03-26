f4111c1fedb3306cc23546d9cc64ccffaebe1343
Your Name <Student_MA117@Student-MA117.local>
2020-03-26 12:09:00 -0400

    trying to fix errors in startCounting function's objects

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -3,17 +3,32 @@ import React from 'react';
 {/* my timer starts at 0 and counts up */ }
 {/* timer needs a reset state and a stop state */ }
 {/* render function first? */}
+{/* https://reactjs.org/docs/react-component.html */}
 {/*  log seconds first using Date.prototype.getSeconds() in a state counting from 0 */}
 {/* realized rendering visual components should be separate */}
 {/*according to examples on w3schools, my starting state should be under class start */}
 {/* https://www.w3schools.com/react/react_state.asp */}
+{/* learned that all script must run before render as well! */}
 
 class Stopwatch extends Component {
-    offState={   //starting at off state, everything should be 0
-        on=false,
-        start=0,
-        time=0,
+    offState={   /*starting at off state, everything should be 0 */
+        on:false,
+        start:0,
+        time:0,
     }
+    /* note for the future, these are objects don't use =, use : for objects inside a function!!! */
+    /* now I need a function to execute the values above, but also run after 0 */
+    startCounting = () => {
+        this.setState (    /* this current time frame should equal to: start running. setState to change it from previous state where it was off */
+            on: true,
+            onCount:this.state.onCount, /* just declaring in case of undefined errors */
+            start: Date.prototype.getSeconds() - this.state.onCount, /* start time from right now till ??? */
+        )
+    }
+
+
+    /* okay pause Idk how to get this in console so let's render this baby on the DOM! css time! */
+
     render(){
         return (
             <div className="Stopwatch">
